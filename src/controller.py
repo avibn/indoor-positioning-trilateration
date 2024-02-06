fc5932f5cf082aa6035c8336eae6854d1aa798b1
Alvis <37454764+avibn@users.noreply.github.com>
2024-02-06 18:51:08 +0000

    add controller

--- src/controller.py
+++ src/controller.py
@@ -1,19 +1,144 @@
 # This will contain the MQTT client and the controller logic
 import asyncio
+from typing import List, Tuple
+
+from bleak import BleakScanner
 
 from calc import get_position
 from image import generate_image_payload
 from libs.bluetooth import Bluetooth
 
+
+class Controller:
+    def __init__(self, address: str = "DC:03:BB:B0:67:4A"):
+        """
+        Initializes the Controller object.
+
+        Args:
+            address (str): The Bluetooth address to connect to. Defaults to "DC:03:BB:B0:67:4A".
+
+        Raises:
+            Exception: If there is an error connecting to the Bluetooth device.
+        """
+
+        self.__background = [[(0, 0, 0) for _ in range(32)] for _ in range(32)]
+        self.__beacons = [(0, 0), (0, 31), (31, 0)]
+        self.__started = False
+        try:
+            self.__bt = Bluetooth(address)
+        except Exception as e:
+            print(f"Could not connect to bluetooth: {e}")
+            raise e
+
+    async def discover(self):
+        """
+        Discovers nearby devices using BleakScanner and prints them. (TODO: Remove this method in the future.)
+        """
+
+        devices = await BleakScanner.discover()
+        for d in devices:
+            print(d)
+
+    async def __image_mode_on(self):
+        """
+        Sends a byte array to enable image mode.
+
+        Raises:
+            Any exceptions that occur during the sending process.
+        """
+
+        await self.__bt.send(
+            bytearray(
+                [
+                    5,
+                    0,
+                    4,
+                    1,
+                    int(1) % 256,
+                ]
+            )
+        )
+
+    async def plot(self, x: int, y: int):
+        """
+        Plot the position (x, y) on the image.
+
+        Parameters:
+        - x (int): The x-coordinate of the position.
+        - y (int): The y-coordinate of the position.
+
+        Returns:
+        None
+        """
+
+        # Ensure image mode is on
+        if not self.__started:
+            await self.__image_mode_on()
+            self.__started = True
+
+        # Send image payload
+        await self.__bt.send(
+            generate_image_payload((x, y), self.__beacons, background=self.__background)
+        )
+
+        print(f"Finished plotting position {x}, {y}")
+
+    async def disconnect(self):
+        """
+        Disconnects from the Bluetooth device.
+        """
+        await self.__bt.disconnect()
+
+    def set_background(self, background: List[List[Tuple[int, int, int]]]):
+        """
+        Set the background of the controller.
+
+        Args:
+            background (List[List[Tuple[int, int, int]]]): A 2D list of tuples representing the RGB values of each pixel.
+
+        Returns:
+            None
+        """
+        self.__background = background
+
+    def set_beacons(self, beacons: List[Tuple[int, int]]):
+        """
+        Set the list of beacons.
+
+        Args:
+            beacons (List[Tuple[int, int]]): A list of tuples representing the coordinates of the beacons.
+
+        Returns:
+            None
+        """
+        self.__beacons = beacons
+
+
 if __name__ == "__main__":
-    # Create a bluetooth connection
-    bt = Bluetooth("00:00:00:00:00:00")
+    bt = Controller("DC:03:BB:B0:67:4A")
+    # bt.set_beacons([(0, 0), (0, 10), (10, 0)])
+
+    # Set the background to look like a room with a bed and table
+    background = [
+        [(200, 200, 200) if i in [0, 31] or j in [0, 31] else (150, 75, 0) if 10 <= i <= 14 and 10 <= j <= 14 else (139, 69, 19) if 20 <= i <= 22 and 20 <= j <= 22 else (255, 255, 255) for j in range(32)] for i in range(32)  # fmt: skip
+    ]
+    bt.set_background(background)
 
     # Get position (example)
     x, y = get_position((0, 0), (0, 10), (10, 0), -88, -86, -69)
 
-    # Send image payload
-    asyncio.run(bt.send(generate_image_payload(x, y)))
+    async def plot_position(x, y):
+        for i in range(20):
+            # testing
+            y = i + 3
 
-    # Disconnect from the bluetooth
-    bt.disconnect()
+            # Plot the position
+            print(f"Plotting position {x}, {y}")
+            await bt.plot(x, y)
+
+            # await asyncio.sleep(2)
+
+        await bt.disconnect()
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(plot_position(x, y))
--- src/image.py
+++ src/image.py
@@ -1,12 +1,15 @@
+import copy
 import io
-from PIL import Image
 import struct
+from typing import List, Tuple
+
+from PIL import Image
 
 pixel_size = 32
 
 
 # https://github.com/derkalle4/python3-idotmatrix-client
-def __create_payloads(png_data):
+def __create_bt_payloads(png_data):
     """Creates payloads from a PNG file.
 
     Args:
@@ -45,37 +48,80 @@ def __create_payloads(png_data):
     return payloads
 
 
-def generate_image_payload(
-    point_x: int, point_y: int, point_color: tuple = (255, 0, 0)
-) -> Image:
-    # todo:: take in an optional background image
-    # Generate a 32x32 2D array of random RGB values
-    image_data = [[(0, 0, 0) for _ in range(32)] for _ in range(32)]
+def __create_img_buffer(
+    image_data: List[List[Tuple[int, int, int]]], rotate: int = 1, size: int = 32
+) -> io.BytesIO:
+    """
+    Create an image buffer from the given image data.
 
-    # make points beacon points white
-    image_data[0][0] = (255, 255, 255)
-    image_data[0][31] = (255, 255, 255)
-    image_data[31][0] = (255, 255, 255)
-    # make point 28, 20 red
-    image_data[point_x][point_y] = point_color
+    Args:
+        image_data (List[List[Tuple[int, int, int]]]): The pixel data of the image.
+        rotate (int, optional): The number of 90-degree rotations to apply to the image. Defaults to 1.
+        size (int, optional): The size of the image. Defaults to 32.
 
-    # Flatten the 2D array to 1D
-    image_data = [pixel for row in image_data for pixel in row]
+    Returns:
+        io.BytesIO: The image buffer as a BytesIO object.
+    """
 
     # Create image
-    img = Image.new("RGB", (32, 32))
+    img = Image.new("RGB", (size, size))
     img.putdata(image_data)
 
     # Rotate image
-    img = img.rotate(90)
+    img = img.rotate(90 * rotate)
 
     # Save the image as bytes
     png_buffer = io.BytesIO()
     img.save(png_buffer, format="PNG")
     png_buffer.seek(0)
 
-    return __create_payloads(png_buffer.getvalue())
+    return png_buffer
+
+
+def generate_image_payload(
+    object_coords: Tuple[int, int],
+    beacon_coords: List[Tuple[int, int]] = [],
+    object_color: tuple = (255, 0, 0),
+    beacon_color: tuple = (0, 255, 0),
+    background: List[List[Tuple[int, int, int]]] = None,
+) -> Image:
+    """
+    Generate an image payload with specified object coordinates, beacon coordinates, point color, and background.
+
+    Parameters:
+        object_coords (Tuple[int, int]): The coordinates of the object point in the image.
+        beacon_coords (List[Tuple[int, int]], optional): The coordinates of the beacon points in the image. Defaults to an empty list.
+        object_color (tuple, optional): The color of the object point. Defaults to (255, 0, 0) (red).
+        beacon_color (tuple, optional): The color of the beacon points. Defaults to (0, 255, 0) (green).
+        background (List[List[Tuple[int, int, int]]], optional): The background image data represented as a 2D array of RGB values. Defaults to black screen.
+
+    Returns:
+        Image: The generated image payload.
+
+    """
+
+    # Generate default 32x32 2D array of RGB values
+    if not background:
+        image_data = [[(0, 0, 0) for _ in range(32)] for _ in range(32)]
+    else:
+        image_data = copy.deepcopy(background)
+
+    # Make points beacon points white
+    for point in beacon_coords:
+        beacon_x, beacon_y = point
+        image_data[beacon_x][beacon_y] = beacon_color
+
+    # Set the object point to the specified color
+    point_x, point_y = object_coords
+    image_data[point_x][point_y] = object_color
+
+    # Flatten the 2D array to 1D
+    image_data = [pixel for row in image_data for pixel in row]
+
+    # Create image buffer
+    png_buffer = __create_img_buffer(image_data)
+    return __create_bt_payloads(png_buffer.getvalue())
 
 
 if __name__ == "__main__":
-    print(generate_image_payload(27, 20))
+    print(generate_image_payload((27, 20), [(0, 0), (0, 10), (10, 0)]))
