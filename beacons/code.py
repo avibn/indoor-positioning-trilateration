655b88a4c292a8b41720d1b6c233746808654e32
Alvis <37454764+avibn@users.noreply.github.com>
2024-03-14 18:35:58 +0000

    filter devices in advertisement

--- beacons/code.py
+++ beacons/code.py
@@ -1,23 +1,24 @@
+import time
 from adafruit_ble import BLERadio
-
 from adafruit_ble.advertising import Advertisement
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 
-ble = BLERadio()
-print("scanning")
-found = set()
-scan_responses = set()
-# By providing Advertisement as well we include everything, not just specific advertisements.
-for advertisement in ble.start_scan(ProvideServicesAdvertisement, Advertisement):
-    addr = advertisement.address
-    if advertisement.scan_response and addr not in scan_responses:
-        scan_responses.add(addr)
-    elif not advertisement.scan_response and addr not in found:
-        found.add(addr)
-    else:
-        continue
-    print(addr, advertisement)
-    print("\t" + repr(advertisement))
-    print()
+# List of addresses to filter
+addresses_to_filter = {"92c850cceee8"}
 
-print("scan done")
+ble = BLERadio()
+
+counter = 0
+
+for advertisement in ble.start_scan(ProvideServicesAdvertisement, Advertisement):
+    addr_bytes = advertisement.address.address_bytes
+    addr_str = "".join("{:02x}".format(b) for b in addr_bytes).lower()
+
+    if addr_str in addresses_to_filter:
+        if counter % 4 == 0:
+            print("RSSI:", advertisement.rssi)
+            print("\tAddress:", addr_str)
+        counter += 1
+
+ble.stop_scan()  # Ensure scanning is stopped before continuing
+print("Scan done.")
